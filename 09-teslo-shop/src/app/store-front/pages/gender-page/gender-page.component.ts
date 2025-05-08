0291937bdb6edeea9b4000d78cb42f0165d1df59
Felipe Vasquez Gomez <felipevasquez1998@gmail.com>
2025-05-07 22:41:17 -0500

    seccion 18 terminada

---
+++ 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+
+
+@Component({
+  selector: 'app-gender-page',
+  imports: [ ProductCardComponent ],
+  templateUrl: './gender-page.component.html',
+})
+export class GenderPageComponent {
+
+route = inject(ActivatedRoute);
+productsService = inject(ProductsService);
+
+gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+
+productsResource = rxResource({
+  request: () => ({ gender: this.gender() }),
+  loader:({ request }) => {
+    return this.productsService.getProducts({
+      gender: request.gender
+    });
+  }
+});
+
+}
