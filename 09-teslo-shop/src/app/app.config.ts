4afb372d940108e7a47cc53eb7b4ab0425f8c7c2
Felipe Vasquez Gomez <felipevasquez1998@gmail.com>
2025-05-24 19:48:04 -0500

    seccion 20 terminada

--- 09-teslo-shop/src/app/app.config.ts
+++ 09-teslo-shop/src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
+import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        // loggingInterceptor,
+        authInterceptor
+      ])
+    )
   ]
 };
--- 09-teslo-shop/src/app/app.routes.ts
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [
+      // () => {
+      //   console.log('hola mundo');
+      //   return true;
+      // },
+      NotAuthenticatedGuard,
+    ]
+  },
   {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes')
